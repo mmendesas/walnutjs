5bfb3c34670b88bfcdb71f74504c9ca582a7e052
Marcio Mendes <marcio.mendes@dafiti.com.br>
2017-02-16 17:50:06 -0200

    Remove the now ignored directory node_modules

--- node_modules/protractor-cucumber-framework/lib/runState.js
+++
@@ -1,9 +0,0 @@
-var state = {};
-
-state.initialize = function (runner, results, strict) {
-  state.runner = runner;
-  state.results = results;
-  state.strict = strict;
-};
-
-module.exports = state;
