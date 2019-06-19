20259611e5e6fb46490164a62194fa43e324ff5d
Marcio Mendes <mmendes.as@gmail.com>
2019-06-18 23:43:57 -0300

    feat:update file methods

--- src/support/helpers/file.js
+++ src/support/helpers/file.js
@@ -1,18 +1,15 @@
 const path = require('path');
-const config = require('../config');
 const fs = require('fs-plus');
 
 module.exports = {
 
-  getTreatedFilename: function (folder_path, name) {
+  getTreatedFilename: (folder_path, name) => {
     const filename = path.join(this.getTreatedPath(config.evidencesPath), folder_path.join('/'), name);
-
     this.ensureDirectoryExistence(filename);
-
     return filename;
   },
 
-  ensureDirectoryExistence: function (filePath) {
+  ensureDirectoryExistence: (filePath) => {
     const dirname = path.dirname(filePath);
 
     if (fs.existsSync(dirname)) {
@@ -22,7 +19,7 @@ module.exports = {
     fs.mkdirSync(dirname);
   },
 
-  getTreatedPath: function (filepath) {
+  getTreatedPath: (filepath) => {
     if (filepath.startsWith('{default}') || filepath == '') {
       filepath = filepath.replace('{default}', '');
       filepath = path.join(process.cwd(), 'test', filepath);
@@ -33,17 +30,17 @@ module.exports = {
     return filepath;
   },
 
-  writePNGToFile: function (data, filename) {
+  writePNGToFile: (data, filename) => {
     const pngStream = fs.createWriteStream(filename + '.png');
     pngStream.write(new Buffer(data, 'base64'));
     pngStream.end();
   },
 
-  readContentFromFile: function (filename) {
+  readContentFromFile: (filename) => {
     return fs.readFileSync(filename, 'utf8');
   },
 
-  writeContentToFile: function (data, filename) {
+  writeContentToFile: (data, filename) => {
     fs.writeFileSync(filename, data, 'utf8');
   }
 }
