14f8d651ca4da0ad9d57724dbe252307aa3784ed
Marcio Mendes <mmendes.as@gmail.com>
2019-06-18 22:01:17 -0300

    feat: update api common steps

--- src/step_defs/api/common-steps.js
+++ src/step_defs/api/common-steps.js
@@ -1,40 +1,36 @@
-var trest = require('../../support/api/client');
-
+const api = require('../../support/api/client');
 const { logger } = helpers;
 
-var commonapi = function () {
-  /**
-   * User prints the current body content (Req|Res)
-   */
-  this.Given(/^\(api\) user prints the current (REQUEST|RESPONSE) body content$/, function (type, callback) {
-    const content = type === 'REQUEST' ? trest.requestContent : trest.response.raw_body;
-    logger.info(`[${type}] content:\n${content}\n`);
-  });
+/**
+ * User prints the current body content (Req|Res)
+ */
+Given(/^\(api\) user prints the current (REQUEST|RESPONSE) body content$/, (type) => {
+  const content = type === 'REQUEST' ? api.requestContent : api.response.raw_body;
+  logger.info(`[${type}] content:\n${content}\n`);
+});
 
-  /**
-   * Define a value for request headers [Accept, Content-Type]
-   */
-  this.Given(/^\(api\) user will send and accept (XML|JSON|HTML)$/, function (type) {
-    var accept = 'application/json';
-    var contentType = 'application/json';
+/**
+ * Define a value for request headers [Accept, Content-Type]
+ */
+Given(/^\(api\) user will send and accept (XML|JSON|HTML)$/, (type) => {
+  const accept = 'application/json';
+  const contentType = 'application/json';
 
-    switch (type) {
-      case 'XML':
-        accept = 'application/xml';
-        contentType = 'application/xml';
-        break;
+  switch (type) {
+    case 'XML':
+      accept = 'application/xml';
+      contentType = 'application/xml';
+      break;
 
-      case 'HTML':
-        accept = 'text/html';
-        contentType = 'application/x-www-form-urlencoded';
-        break;
+    case 'HTML':
+      accept = 'text/html';
+      contentType = 'application/x-www-form-urlencoded';
+      break;
 
-      default:
-        break;
-    }
-    trest.addHeader('Accept', accept);
-    trest.addHeader('Content-Type', contentType);
-  });
-};
+    default:
+      break;
+  }
+  api.addHeader('Accept', accept);
+  api.addHeader('Content-Type', contentType);
+});
 
-module.exports = commonapi;
