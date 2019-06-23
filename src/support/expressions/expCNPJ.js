01dbbfa6f3bd331a4d581ed34dd6151bddc6b2c8
Marcio Mendes <mmendes.as@gmail.com>
2019-06-23 20:01:17 -0300

    feat: update expression CNPJ

--- src/support/expressions/expCNPJ.js
+++ src/support/expressions/expCNPJ.js
@@ -1,83 +1,81 @@
-var helperString = require('../helpers/string');
+const string = require('../helpers/string');
 
-var expCNPJ = {
+/**
+ * Used to generate a valid CNPJ with or without format
+ */
+const parseExpression = (expression) => {
+  if (expression != '' && expression != 'f') {
+    var sb = [];
 
-    /**
-     * Used to generate a valid CNPJ with or without format
-     */
-  parseExpression: function (expression) {
-    if (expression != '' && expression != 'f') {
-      var sb = [];
+    sb.push('[1] - cnpj()  - 19445769000141');
+    sb.push('[2] - cnpj(f) - 19.445.769/0001-41');
 
-      sb.push('[1] - cnpj()  - 19445769000141');
-      sb.push('[2] - cnpj(f) - 19.445.769/0001-41');
-
-      throw sb.join('\n');
-    }
-
-    var cnpjResult = this.generateCNPJ();
-
-    if (expression === '') { return cnpjResult }
-
-    if (expression === 'f') { return helperString.formatString('{0}{1}.{2}{3}{4}.{5}{6}{7}/{8}{9}{10}{11}-{12}{13}', cnpjResult) }
-  },
-
-    /**
-     * Generate the valid CNPJ
-     */
-  generateCNPJ: function () {
-    var ini = '';
-    var num = 0;
-
-    for (var i = 0; i < 8; i++) {
-      num = Math.floor((Math.random() * 10));
-      ini += num.toString();
-    }
-
-    ini += '0001';
-
-    return ini + this.calculateLastNums(ini);
-  },
-
-    /**
-     * Calculate the last two characteres
-     */
-  calculateLastNums: function (num) {
-    var num01 = 0, num02 = 0;
-    var sum = 0, weight = 5;
-
-    for (var i = 0; i < 4; i++) {
-      sum += parseInt(num.substring(i, i + 1)) * weight--;
-    }
-
-    weight = 9;
-
-    for (var i = 4; i < num.length; i++) {
-      sum += parseInt(num.substring(i, i + 1)) * weight--;
-    }
-
-    if (sum % 11 == 0 | sum % 11 == 1) { num01 = 0 } else { num01 = 11 - (sum % 11) }
-
-    sum = 0;
-    weight = 6;
-
-
-    for (var i = 0; i < 5; i++) {
-      sum += parseInt(num.substring(i, i + 1)) * weight--;
-    }
-
-    weight = 9;
-
-    for (var i = 5; i < num.length; i++) {
-      sum += parseInt(num.substring(i, i + 1)) * weight--;
-    }
-
-    sum += parseInt(num01) * 2;
-
-    if (sum % 11 == 0 | sum % 11 == 1) { num02 = 0 } else { num02 = 11 - (sum % 11) }
-
-    return num01.toString() + num02.toString();
+    throw sb.join('\n');
   }
+
+  const cnpjResult = generateCNPJ();
+
+  if (expression === '') { return cnpjResult }
+  if (expression === 'f') { return string.formatString('{0}{1}.{2}{3}{4}.{5}{6}{7}/{8}{9}{10}{11}-{12}{13}', cnpjResult) }
 };
 
-module.exports = expCNPJ;
+/**
+ * Generate the valid CNPJ
+ */
+const generateCNPJ = () => {
+  var ini = '';
+  var num = 0;
+
+  for (var i = 0; i < 8; i++) {
+    num = Math.floor((Math.random() * 10));
+    ini += num.toString();
+  }
+
+  ini += '0001';
+
+  return ini + calculateLastNums(ini);
+};
+
+/**
+ * Calculate the last two characteres
+ */
+const calculateLastNums = (num) => {
+  let num01 = 0, num02 = 0;
+  let sum = 0, weight = 5;
+
+  for (let i = 0; i < 4; i++) {
+    sum += parseInt(num.substring(i, i + 1)) * weight--;
+  }
+
+  weight = 9;
+
+  for (let i = 4; i < num.length; i++) {
+    sum += parseInt(num.substring(i, i + 1)) * weight--;
+  }
+
+  if (sum % 11 == 0 | sum % 11 == 1) { num01 = 0 } else { num01 = 11 - (sum % 11) }
+
+  sum = 0;
+  weight = 6;
+
+
+  for (let i = 0; i < 5; i++) {
+    sum += parseInt(num.substring(i, i + 1)) * weight--;
+  }
+
+  weight = 9;
+
+  for (let i = 5; i < num.length; i++) {
+    sum += parseInt(num.substring(i, i + 1)) * weight--;
+  }
+
+  sum += parseInt(num01) * 2;
+
+  if (sum % 11 == 0 | sum % 11 == 1) { num02 = 0 } else { num02 = 11 - (sum % 11) }
+
+  return num01.toString() + num02.toString();
+};
+
+module.exports = {
+  parseExpression
+};
--- test/support/expressions/expCNPJ.test.js
+++ test/support/expressions/expCNPJ.test.js
@@ -1,17 +1,17 @@
 const interpreter = require('../../../src/support/expressions/interpreter');
 
 describe('CNPJ Tests', () => {
-    it('should be validate the expression CNPJ', () => {
-        var result = interpreter.resolveExpression('cnpj()');
-        expect(result).toContain('0001');
-        expect(result.length).toEqual(14);
-    });
+  it('should be validate the expression CNPJ', () => {
+    const result = interpreter.resolveExpression('cnpj()');
+    expect(result).toContain('0001');
+    expect(result.length).toEqual(14);
+  });
 
-    it('should be validate the expression CNPJ with format', () => {
-        var result = interpreter.resolveExpression('cnpj(f)');
-        expect(result.length).toEqual(18);
-        expect(result).toContain('/0001');
-        expect(result).toContain('-');
-        expect(result).toContain('.');
-    });
+  it('should be validate the expression CNPJ with format', () => {
+    const result = interpreter.resolveExpression('cnpj(f)');
+    expect(result.length).toEqual(18);
+    expect(result).toContain('/0001');
+    expect(result).toContain('-');
+    expect(result).toContain('.');
+  });
 });
